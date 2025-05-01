f4534872f81ea3d22bd427fae3b59ed95279c814
crjanisca <crjanisca@gmail.com>
2025-05-01 09:03:14 -0500

    Create testing.py

---
+++ Python/testing.py
@@ -0,0 +1,7 @@
+fruits = ["apple", "orange", "banana", "coconut"]
+
+print(dir(fruits)) #prints all the functions that can be used with lists
+print(help(fruits)) #prints a guide on lists
+
+for fruit in fruits:
+    print(fruit)
