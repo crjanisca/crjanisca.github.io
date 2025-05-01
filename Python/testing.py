a592fea4e0015d510786f5da516cff8be3770ef5
crjanisca <crjanisca@gmail.com>
2025-05-01 16:15:31 -0500

    Update testing.py

--- Python/testing.py
+++
@@ -1,7 +0,0 @@
-fruits = ["apple", "orange", "banana", "coconut"]
-
-print(dir(fruits)) #prints all the functions that can be used with lists
-print(help(fruits)) #prints a guide on lists
-
-for fruit in fruits:
-    print(fruit)
