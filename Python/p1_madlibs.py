0bf93ccb741b9b8d425093830dd652820de57fa3
crjanisca <crjanisca@gmail.com>
2025-05-01 09:03:00 -0500

    Create p1_madlibs.py

---
+++ Python/p1_madlibs.py
@@ -0,0 +1,11 @@
+# Madlibs
+
+noun1 = input("Enter a noun: ")
+noun_place1 = input("Enter a place: ")
+verb1 = input("Enter a verb: ")
+adjective_ly1 = input("Enter an adjective using ly: ")
+noun2 = input("Enter a noun: ")
+
+print(f"One time I saw a {noun1} at a {noun_place1}.")
+print(f"I decided to {verb1} after the {noun1},")
+print(f"but I {adjective_ly1} ran into a {noun2}.")
