28991a5ac9446a1011abe200d0fcfb2479c79b80
crjanisca <crjanisca@gmail.com>
2025-05-05 20:26:45 -0500

    Create p9_concession_stand_program.py

---
+++ Python/p9_concession_stand_program.py
@@ -0,0 +1,13 @@
+# Concession Stand Program
+
+menu = {"pizza": 3.00,
+        "nachos": 4.50,
+        "popcorn": 2.00,
+        "fries": 2.50,
+        "chips": 0.75,
+        "pretzel": 3.00,
+        "soda": 2.25,
+        "lemonade": 3.00}
+
+cart = []
+item = menu.key
