79f88ef2a418d4de98c39ccfd0afd286dd51f80a
crjanisca <crjanisca@gmail.com>
2025-05-05 20:26:47 -0500

    Update testing.py

---
+++ Python/testing.py
@@ -0,0 +1,7 @@
+capitals = {"USA": "Washington D.C.",
+            "Pakistan": "Islamabad",
+            "Nepal": "Kathmandu",
+            "Sri Lanka": "Colombo"}
+
+for key, value in capitals.items():
+    print(f"{key}: {value}")
